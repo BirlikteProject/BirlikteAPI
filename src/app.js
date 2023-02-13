71f3ff2ac817685c4ad4fbb273c97791573842ce
mkarademir69 <karademirbayburt@gmail.com>
2023-02-13 19:47:50 +0300

    changed port

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ app.use("/api/v1/upload",require("./routes/upload"));
 app.use("/cities",require("./routes/cities"));
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 443;
 app.listen(PORT, () => {
     console.log("Server is running: "+PORT);
   });
